b30ad9179b7d4e3096b7f405379ab6448ac46871
cauaacosta <cauaa031@gmail.com>
2026-01-16 01:55:46 -0300

    Fim de Seção

--- JS/seção1/aula15/index.js
+++ JS/seção1/aula15/index.js
@@ -1,5 +1,5 @@
 //Numbers em JS
-
+// IEEE 754-2008 padrão do JS para números
 let num1 = 1;
 let num2 = 2.5;
 
@@ -8,5 +8,3 @@ console.log(num1.toString(2)); // mostra a representação binária desse númer
 console.log(num1.toFixed(2)); // mostra a quantidade de casas decimais definidas no parenteses
 console.log(Number.isInteger(num1)); //mostra se é um número inteiro (true/false)
 
-let temp = (num1.toString) + "Olá"
-console.log(temp);
---
+++ JS/seção1/aula21/primitive.js
@@ -0,0 +1,8 @@
+// Aula sobre Dados Primitivos
+
+/*
+  Primitivos(imutaveis) - string, number, boolean, undefined,
+  null (bigint, symbol) - Valores são copiados
+
+  Referência(mutavel) - arrays, objects, function - Passados por referência
+ */
