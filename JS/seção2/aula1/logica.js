7cd509469171545fb26b0dd5da50411ba0d6bfa1
cauaacosta <cauaa031@gmail.com>
2026-01-29 02:44:57 -0300

    Introdução a lógica de JS

---
+++ JS/seção2/aula1/logica.js
@@ -0,0 +1,14 @@
+/*
+Operadores de Comparação
+
+> maior que
+>= maior ou igual a
+< menor que
+<= menor ou igual a
+== igualdade de (valor)
+=== igualdade de (valor e tipo)
+!= diferente de (valor)
+!== diferente de (valor e tipo)
+*/
+
+console.log(10>5);
