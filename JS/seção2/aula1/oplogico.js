607b1c212a0e70c547fef2bce28c657714ea5cbb
cauaacosta <cauaa031@gmail.com>
2026-01-29 02:59:13 -0300

    Operadores Lógicos e de Comparação

---
+++ JS/seção2/aula1/oplogico.js
@@ -0,0 +1,14 @@
+/*
+Operadores Lógicos
+&& -> AND -> E // Todas expressões precisam ser verdadeiras para retornar true
+|| -> OR -> OU // Se uma expressão for verdadeira retorna true
+! -> NOT -> NÂO // Inverte o resultado e retorna o valor contrário
+*/
+const expressaoAnd = true && true && true
+console.log(expressaoAnd);
+
+const expressaoOR = true || false || false
+console.log(expressaoOR);
+
+const expressaoNOT = true || false || false
+console.log(!expressaoNOT); 
